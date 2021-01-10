5940c61773c55b063eb5c7885e9f52e6cd3f55cb
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2021-01-10 18:29:19 -0500

    Landing page, and 404 handling done

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>UI-Pro</h1>
+        <p className="text-muted">Everyone calls their design user friendly till the reviews come in....</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/containers/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="NotFound text-center">
+      <h3>Even Sherlock couldn't find the page you're looking for...</h3>
+    </div>
+  );
+}

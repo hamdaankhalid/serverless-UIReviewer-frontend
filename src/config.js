f231fa86e4d8993ab7e07d633b76ad988d0d580c
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2021-01-10 20:42:36 -0500

    Setting up Auth

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 const config = {
     s3: {
-      REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
-      BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME",
+      REGION: "us-east-1",
+      BUCKET: "notes-app-upload-hk",
     },
     apiGateway: {
-      REGION: "YOUR_API_GATEWAY_REGION",
-      URL: "YOUR_API_GATEWAY_URL",
+      REGION: "us-east-1",
+      URL: "https://console.aws.amazon.com/apigateway/home?region=us-east-1#/apis/2c3sbl8w5l/resources",
     },
     cognito: {
-      REGION: "YOUR_COGNITO_REGION",
-      USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-      APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-      IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID",
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_xs1tKmKY9",
+      APP_CLIENT_ID: "5hmrig9or6mh5s4ng1gr6va3hn",
+      IDENTITY_POOL_ID: "us-east-1:225085b8-6baf-4697-82fa-991dd9903cbf",
     },
   };
   
